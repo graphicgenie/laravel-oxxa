f822c199e00f00b4054b3769a352ec9ee25f4e68
Ronald Gerssen <ronald@graphicgenie.nl>
2022-09-17 12:37:20 +0200

    WIP

--- src/LaravelOxxa.php
+++ src/LaravelOxxa.php
@@ -4,9 +4,4 @@ namespace GraphicGenie\LaravelOxxa;
 
 class LaravelOxxa
 {
-    public static function __callStatic($class, $args)
-    {
-        dd($class, $args);
-        return new $class($args);
-    }
 }
